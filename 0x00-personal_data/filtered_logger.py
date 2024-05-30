7064976967bedf6e440de48e092e752f4bb0bb17
Keglo Dodzi <stephanekeglo@gmail.com>
2024-05-30 23:32:19 +0000

    Add Task #1 files

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """filter_datum
 """
+import logging
 import re
-from typing import List
+from typing import List, Tuple
 
 
 def filter_datum(fields: List[str],
@@ -12,3 +13,24 @@ def filter_datum(fields: List[str],
     """Retuns the log message obfuscated"""
     pattern = '|'.join([rf'(?<={field}=)[^{separator}]+' for field in fields])
     return re.sub(pattern, redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: Tuple[str] = None):
+        """Initializer"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filter value in log records"""
+        record.message = filter_datum(self.fields, self.REDACTION,
+                                      super().format(record),
+                                      self.SEPARATOR)
+        return record.message
