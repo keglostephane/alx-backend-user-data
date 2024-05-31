7b5bf7aefe4405a2f1493326d6a70f5627a984af
Keglo Dodzi <stephanekeglo@gmail.com>
2024-05-31 05:59:59 +0000

    Add encrypt_password.py

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Hash_password Module
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Returns a salted, hashed password, which is a byte string
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    matches check between passwords
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
