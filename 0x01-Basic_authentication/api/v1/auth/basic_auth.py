bb8f6de6efc436c0313d9a735a91eb0fc10394a1
Keglo Dodzi <stephanekeglo@gmail.com>
2024-06-06 21:23:59 +0000

    Add support for   character in password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -49,7 +49,8 @@ class BasicAuth(Auth):
             return None, None
         if ":" not in decoded_base64_authorization_header:
             return None, None
-        return tuple(decoded_base64_authorization_header.split(':'))
+        return tuple(decoded_base64_authorization_header.split(':',
+                                                               maxsplit=1))
 
     def user_object_from_credentials(self,
                                      user_email: str,
