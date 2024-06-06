3859c94524e55f74e106f24d4d26c06fe1e9397d
Keglo Dodzi <stephanekeglo@gmail.com>
2024-06-06 21:46:35 +0000

    Add support for wildcard at the end of excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Auth module
 """
+import re
 from flask import request
 from typing import List, TypeVar
 
@@ -15,8 +16,12 @@ class Auth:
         if path is None or excluded_paths is None:
             return True
         for endpoint in excluded_paths:
-            if endpoint[:-1] == path or endpoint == path:
-                return False
+            if '*' not in endpoint:
+                if endpoint[:-1] == path or endpoint == path:
+                    return False
+            else:
+                if path.startswith(endpoint[:-1]):
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
