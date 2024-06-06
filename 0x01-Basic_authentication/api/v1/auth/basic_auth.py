dcbf1ec3f8ce5016d22b3c3b00f8139e65c15928
Keglo Dodzi <stephanekeglo@gmail.com>
2024-06-06 14:44:50 +0000

    implements extract_user_credentials method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,3 +33,15 @@ class BasicAuth(Auth):
         except binascii.Error:
             return None
         return decoded.decode('utf-8')
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """Returns user email and password from Base64 decoded value"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        return tuple(decoded_base64_authorization_header.split(':'))
