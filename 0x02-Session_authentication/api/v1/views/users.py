6465479ec5630e738e21e643cf2e4b44a4e7ee60
Keglo Dodzi <stephanekeglo@gmail.com>
2024-06-08 17:09:38 +0000

    Fix #6

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -30,7 +30,7 @@ def view_one_user(user_id: str = None) -> str:
     if user_id is None:
         abort(404)
     if user_id == "me" and request.current_user is None:
-        abort(404)
+        abort(403)
     if user_id == "me" and request.current_user is not None:
         user = User.get(request.current_user.id)
         return jsonify(user.to_json())
