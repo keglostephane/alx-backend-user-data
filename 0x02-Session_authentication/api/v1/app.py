2255f8a8188caf487cd85de2b1f3b4c1f68c9950
Keglo Dodzi <stephanekeglo@gmail.com>
2024-06-09 14:32:04 +0000

    Don't handle auth authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -14,10 +14,8 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = getenv("AUTH_TYPE", None)
 
-if auth == 'auth':
-    from api.v1.auth.auth import Auth
-    auth = Auth()
-elif auth == 'basic_auth':
+
+if auth == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 elif auth == 'session_auth':
