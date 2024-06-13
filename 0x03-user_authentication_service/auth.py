de9e997ca6bbacaab162126b9c7baec72477351a
Keglo Dodzi <stephanekeglo@gmail.com>
2024-06-13 23:59:02 +0000

    Add create_session method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -48,3 +48,14 @@ class Auth:
             return False
 
         return checkpw(password.encode('utf-8'), user.hashed_password)
+
+    def create_session(self, email: str) -> str:
+        """create a session and return the session ID.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            self._db.update_user(user.id, session_id=_generate_uuid())
+        except NoResultFound:
+            return None
+
+        return user.session_id
