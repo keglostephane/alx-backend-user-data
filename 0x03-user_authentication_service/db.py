9116099b6b68fa747f9c6df3b80a13bf2446da9f
Keglo Dodzi <stephanekeglo@gmail.com>
2024-06-13 08:21:10 +0000

    Implement find_user_by method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,9 +5,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base
 from user import User
+from typing import Dict
 
 
 class DB:
@@ -41,3 +44,13 @@ class DB:
         self._session.commit()
 
         return user
+
+    def find_user_by(self, **kwargs: Dict) -> User:
+        """Retrieve the first user matching keyword argument"""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+        except NoResultFound:
+            raise NoResultFound("No result found")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid request")
