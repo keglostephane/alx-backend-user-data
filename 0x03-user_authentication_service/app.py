47cada0f55038a93d442cf53b03f2c109117fb69
Keglo Dodzi <stephanekeglo@gmail.com>
2024-06-14 05:14:34 +0000

    Add /sessions route with DELETE method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask App
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 from typing import Dict
 
@@ -46,6 +46,20 @@ def login() -> Dict:
     resp.set_cookie('session_id', session_id)
 
     return resp
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """Log out a user
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if not user:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
+    redirect('/')
 
 
 if __name__ == "__main__":
