1a47ab3643df83e1a1fff2a7c839770d4a6a4103
Keglo Dodzi <stephanekeglo@gmail.com>
2024-06-14 08:21:20 +0000

    Fix #9

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -79,10 +79,11 @@ def get_reset_password_token() -> str:
     """
     email = request.form.get('email')
     try:
-        reset_token = Auth.get_reset_password_token(email)
-        return jsonify({"email": email, "reset_token": reset_token}), 200
-    except Exception:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
         abort(403)
+
+    jsonify({"email": email, "reset_token": reset_token})
 
 
 if __name__ == "__main__":
