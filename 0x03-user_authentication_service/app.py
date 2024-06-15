86724837a8873d68f4468d42d5b8651ff6fb9fe0
Keglo Dodzi <stephanekeglo@gmail.com>
2024-06-15 01:07:15 +0000

    Fix /reset_password post method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -80,7 +80,7 @@ def get_reset_password_token() -> str:
     email = request.form.get('email')
     try:
         reset_token = AUTH.get_reset_password_token(email)
-    except Exception:
+    except ValueError:
         abort(403)
 
     return jsonify({"email": email, "reset_token": reset_token})
